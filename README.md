e1fd638fad2c1efdf0b50398186da9c5b7c9bfc3
synbyte <81550106+synbyte@users.noreply.github.com>
2022-10-12 21:55:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-g6waqv
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/synbyte/nativescript-stackblitz-templates-g6waqv)
---
+++ app/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Home" class="font-bold text-lg" />
+    </ActionBar>
+
+    <GridLayout>
+      <Label
+        class="text-xl align-middle text-center text-gray-500"
+        :text="message"
+        @tap="logMessage"
+      />
+      <Button @click="batLevel" :text="test" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+
+export default Vue.extend({
+  computed: {
+    message() {
+      return 'Blank {N}-Vue app';
+    },
+  },
+
+  methods: {
+    logMessage() {
+      console.log('You have tapped the message!');
+    },
+    batLevel() {
+      let value = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+      alert(formatMessage(value));
+    },
+  },
+  
+});
+</script>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
